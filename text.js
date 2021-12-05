5e7e470920c68b7a62fb7e0dd0ca0da3537da68f
SKALEZ-A <holumacbeth@gmail.com>
2021-12-05 21:47:23 +0100

    latest

---
+++ text.js
@@ -0,0 +1,6 @@
+//how to make the alert pop up on your screen
+
+document.getElementById('submit').addEventListener('click', function(){
+  alert('are you sure you want to submit your name');
+});
+
